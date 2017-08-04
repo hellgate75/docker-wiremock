d8c43c13290c40a8f5b3772ab4c5bbdce1532646
hellgate75 <hellgate75@gmail.com>
2017-08-04 19:42:29 +0100

    Further test data

--- zookeeper/client-log-zookeeper.sh
+++ zookeeper/client-log-zookeeper.sh
@@ -4,5 +4,8 @@ function system_log() {
   mkdir -p /var/log/zookeeper
   echo "[$(date)] $1" >> /var/log/zookeeper/logging-client.log
 }
-
 system_log "Zookepeer Logging Client running ..."
+zkCli.sh create $CURRENT_SERVER_PATH
+zkCli.sh set $CURRENT_SERVER_PATH "$(date)"
+zkCli.sh create $CURRENT_SERVER_PATH/$CURRENT_SERVER_ID
+zkCli.sh set $CURRENT_SERVER_PATH/$CURRENT_SERVER_ID "$(date)"
